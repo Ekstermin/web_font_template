6cc8ea8384d0a962f5e3ce8e5e53596d9713613a
Dominik Winiarek <dominik.winiarek@wp.pl>
2022-03-09 19:04:45 +0100

    my font template

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	
+	<link href="fonts/fontawesome/css/all.css" rel="stylesheet">
+  
+	<link rel="stylesheet" href="fonts/style-fonts.css" type="text/css">
+    <link rel="stylesheet" href="style/main.css" type="text/css">
+	
+	
+    <title>TEST</title>
+</head>
+<body>
+   <p>testęeeeeeeeeeeeeeeee</p> 
+   <i class="fas fa-home"></i>
+</body>
+</html>
